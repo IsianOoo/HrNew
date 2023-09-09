97ca72d9aa763c62ca638369175bf6d3e77995b3
Krystian <Krystian@DESKTOP-DA1HJHA>
2023-09-09 17:04:03 +0200

    add dbcontext

---
+++ HrNew.Identity/HrManagementIdentityDbContext.cs
@@ -0,0 +1,29 @@
+﻿using HrNew.Identity.Configurations;
+using HrNew.Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HrNew.Identity
+{
+    public class HrManagementIdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public HrManagementIdentityDbContext(DbContextOptions<HrManagementIdentityDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new RoleConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new UserRoleConfiguration());
+        }
+    }
+}
