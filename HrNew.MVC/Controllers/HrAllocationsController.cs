1c83c1b7168da2f516fbdbe9bd996194bdc4126e
Krystian <Krystian@DESKTOP-DA1HJHA>
2023-09-10 11:17:32 +0200

    add allocations and request controllers

---
+++ HrNew.MVC/Controllers/HrAllocationsController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace HrNew.MVC.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class HrAllocationsController : Controller
+    {
+        private readonly IHrAllocationService _deliveryAllocationRepository;
+
+        public HrAllocationsController(IHrAllocationService deliveryAllocationRepository)
+        {
+            _deliveryAllocationRepository = deliveryAllocationRepository;
+        }
+
+        // GET: HrAllocationsController
+        public async Task<ActionResult> Index()
+        {
+            var model = await _deliveryAllocationRepository.GetHrAllocations();
+            return View(model);
+        }
+
+        // GET: HrAllocationsController/Details/5
+        public async Task<ActionResult> Details(int id)
+        {
+            var model = await _deliveryAllocationRepository.GetHrAllocationDetails(id);
+            return View(model);
+        }
+
+        // GET: HrAllocationsController/Create
+        public async Task<ActionResult> Create()
+        {
+            return View();
+        }
+
+        // POST: HrAllocationsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(CreateHrAllocationVM deliveryAllocation)
+        {
+            try
+            {
+                var response = await _deliveryAllocationRepository.CreateHrAllocation(deliveryAllocation);
+                if (response.Success)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                ModelState.AddModelError("", response.ValidationErrors);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+            }
+            return View(deliveryAllocation);
+        }
+
+        // GET: HrAllocationsController/Edit/5
+        public async Task<ActionResult> Edit(int id)
+        {
+            var model = await _deliveryAllocationRepository.GetHrAllocationDetails(id);
+            return View(model);
+        }
+
+        // POST: HrAllocationsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(int id, HrAllocationVM deliveryAllocation)
+        {
+            try
+            {
+                var response = await _deliveryAllocationRepository.UpdateHrAllocation(id, deliveryAllocation);
+                if (response.Success)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                ModelState.AddModelError("", response.ValidationErrors);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+            }
+            return View(deliveryAllocation);
+        }
+
+        // POST: HrAllocationsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+                var response = await _deliveryAllocationRepository.DeleteHrAllocation(id);
+                if (response.Success)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                ModelState.AddModelError("", response.ValidationErrors);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+            }
+            return View();
+        }
+    }
+}
---
+++ HrNew.MVC/Controllers/HrRequestsController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HrNew.MVC.Controllers
+{
+    public class HrRequestsController : Controller
+    {
+        private readonly IHrRequestService _hrRequestRepository;
+
+        public HrRequestsController(IHrRequestService hrRequestRepository)
+        {
+            _hrRequestRepository = hrRequestRepository;
+        }
+
+        // GET: HrRequestsController
+        public async Task<ActionResult> Index()
+        {
+            var model = await _hrRequestRepository.GetHrRequests();
+            return View(model);
+        }
+
+        // GET: HrRequestsController/Details/5
+        public async Task<ActionResult> Details(int id)
+        {
+            var model = await _hrRequestRepository.GetHrRequestsDetails(id);
+            return View(model);
+        }
+
+        // GET: HrRequestsController/Create
+        public async Task<ActionResult> Create()
+        {
+            return View();
+        }
+
+        // POST: HrRequestsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(CreateHrRequestVM hrRequest)
+        {
+            try
+            {
+                var response = await _hrRequestRepository.CreateHrRequest(hrRequest);
+                if (response.Success)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                ModelState.AddModelError("", response.ValidationErrors);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+            }
+            return View(hrRequest);
+        }
+
+        // GET: HrRequestsController/Edit/5
+        public async Task<ActionResult> Edit(int id)
+        {
+            var model = await _hrRequestRepository.GetHrRequestsDetails(id);
+            return View(model);
+        }
+
+        // POST: HrRequestsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(int id, HrRequestVM hrRequest)
+        {
+            try
+            {
+                var response = await _hrRequestRepository.UpdateHrRequest(id, hrRequest);
+                if (response.Success)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                ModelState.AddModelError("", response.ValidationErrors);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+            }
+            return View(hrRequest);
+        }
+
+        // GET: HrRequestsController/Delete/5
+        //public async Task<ActionResult> Delete(int id)
+        //{
+        //    var model = await _hrRequestRepository.DeleteHrRequest(id);
+        //    return View();
+        //}
+
+        // POST: HrRequestsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+                var response = await _hrRequestRepository.DeleteHrRequest(id);
+                if (response.Success)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                ModelState.AddModelError("", response.ValidationErrors);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+            }
+            return View();
+        }
+    }
+}
